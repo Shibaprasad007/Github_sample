a6f4198f98ff3382341b1dce6a75f783ce55c0ea
SHIBAPRASAD <SHIBAPRASAD@DESKTOP-56B79RC>
2025-01-08 21:02:29 +0530

    [Shibaprasad]: Created class print mssge gitDemo

---
+++ src/Github_sample/Git_prac.java
@@ -0,0 +1,10 @@
+package Github_sample;
+
+public class Git_prac {
+public static void main(String[] args) {
+	
+	System.out.println("Github practice");
+	System.out.println("Github upload");
+	
+}
+}
